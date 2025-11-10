21d7aeff562b04531a0d414e32364a6329b5d0cc
Tamara Costa <tamara_acds@hotmail.com>
2025-11-10 14:05:56 -0300

    Projeto final - MauiAppHotel

--- MauiAppHotel/App.xaml.cs
+++ MauiAppHotel/App.xaml.cs
@@ -1,8 +1,37 @@
-﻿
+﻿using MauiAppHotel.Models;
+
 namespace MauiAppHotel
 {
     public partial class App : Application
     {
+        public List<Quarto> lista_quartos = new List<Quarto>()
+        {
+            new Quarto()
+            {
+                Descricao = "Suíte Ilha Fiji",
+                ValorDiariaAdulto = 2100.50,
+                ValorDiariaCrianca = 1050.25
+            },
+            new Quarto()
+            {
+                Descricao = "Suíte Brisa do Mar",
+                ValorDiariaAdulto = 1548.50,
+                ValorDiariaCrianca = 774.25
+            },
+            new Quarto()
+            {
+                Descricao = "Suíte Pé na Areia",
+                ValorDiariaAdulto = 1298.00,
+                ValorDiariaCrianca = 649.00
+            },
+            new Quarto()
+            {
+                Descricao = "Suíte Barquinho",
+                ValorDiariaAdulto = 698.00,
+                ValorDiariaCrianca = 349.00
+            }
+        };
+
         public App()
         {
             InitializeComponent();
